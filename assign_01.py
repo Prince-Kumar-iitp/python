54f34813854a631d247d9ec94407310b850dba40
Prince Kumar <prince_2401ai38@iitp.ac.in>
2025-03-28 21:49:00 +0530

    Add files via upload

---
+++ assign_01.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+
+x = input("Enter Your first name: ")
+y = input("Enter Your last name: ")
+print(f"Hello, {x} {y}! Welcome to the Python program.")
