79c4677a345ec55a5a6243983c1b23f04149704a
Prince Kumar <prince_2401ai38@iitp.ac.in>
2025-03-28 22:12:27 +0530

    Add files via upload

---
+++ assign_02.py
@@ -0,0 +1,14 @@
+# Task 01:
+
+x = int(input("Enter a number: "))
+if(x%2==0):
+    print(x, " is an even number.")
+else:
+    print(x, " is an odd number.")
+
+# Task 02:
+b = 0
+for a in range(50):
+    b += a
+print("The sum of numbers from 1 to 50 is: ", b)
+
